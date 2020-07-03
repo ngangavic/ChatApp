a499265a272b61e1ebffaf3796b5fd2850620772
ngangavic <ngangavictor10@gmail.com>
2020-07-03 19:43:16 +0300

    main activity UI

--- app/src/main/java/com/ngangavictor/mychat/MainActivity.kt
+++ app/src/main/java/com/ngangavictor/mychat/MainActivity.kt
@@ -2,10 +2,28 @@ package com.ngangavictor.mychat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViewReceiver:TextView
+    private lateinit var editTextMessage:EditText
+    private lateinit var imageButtonSend:ImageButton
+    private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var recyclerViewMessages: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewReceiver=findViewById(R.id.textViewReceiver)
+        recyclerViewMessages=findViewById(R.id.recyclerViewMessages)
+        floatingActionButton=findViewById(R.id.floatingActionButton)
+        imageButtonSend=findViewById(R.id.imageButtonSend)
+        editTextMessage=findViewById(R.id.editTextMessage)
     }
 }
