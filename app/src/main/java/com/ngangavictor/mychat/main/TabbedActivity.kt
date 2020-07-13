83db1a05485b3393620b1426e4b2c28a474de2b8
ngangavic <ngangavictor10@gmail.com>
2020-07-13 19:27:22 +0300

    UI change

--- app/src/main/java/com/ngangavictor/mychat/main/TabbedActivity.kt
+++ app/src/main/java/com/ngangavictor/mychat/main/TabbedActivity.kt
@@ -21,11 +21,5 @@ class TabbedActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
