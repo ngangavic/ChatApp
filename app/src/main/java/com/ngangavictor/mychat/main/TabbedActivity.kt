e78849057a271a1198d52fb810516ceef0ff5a34
ngangavic <ngangavictor10@gmail.com>
2020-07-13 19:49:09 +0300

    added menu to tabbed activity

--- app/src/main/java/com/ngangavictor/mychat/main/TabbedActivity.kt
+++ app/src/main/java/com/ngangavictor/mychat/main/TabbedActivity.kt
@@ -1,5 +1,6 @@
 package com.ngangavictor.mychat.main
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,10 +9,17 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+
+import com.google.firebase.auth.FirebaseAuth
 import com.ngangavictor.mychat.R
 import com.ngangavictor.mychat.main.ui.main.SectionsPagerAdapter
+import com.ngangavictor.mychat.signin.SignInActivity
 
 class TabbedActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +29,33 @@ class TabbedActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+        toolbar =findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                auth.signOut()
+                startActivity(Intent(this, SignInActivity::class.java))
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
